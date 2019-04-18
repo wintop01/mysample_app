ffcb49651250314d60b8874474863dad55cbdf1d
wintop01 <davidcdx@163.com>
2019-04-18 21:18:47 +0800

    Complete Static pages

---
+++ app/controllers/statis_pages_controller.rb
@@ -0,0 +1,13 @@
+class StatisPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'statis_pages#home'
+
+  get 'statis_pages/home'
+
+  get 'statis_pages/help'
+
+  get 'statis_pages/about'
+
+  get 'statis_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
